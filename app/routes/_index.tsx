95b38f707c6802947bb1e7f102c456d3db125d69
Damien Dumontet <damien.dumontet@protonmail.com>
2023-12-13 12:45:25 -0300

    Start integration landing page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,29 +1,64 @@
-import { json, type LoaderFunctionArgs } from '@remix-run/node';
-import { Form, useLoaderData } from '@remix-run/react';
-import { checkNotAuthenthicatedAndRedirect } from '~/utils/authentication.server';
-
-export async function loader({ request }: LoaderFunctionArgs) {
-  const session = await checkNotAuthenthicatedAndRedirect(request, '/signin');
-  return json(session.user);
-}
+import { ArrowRight } from 'lucide-react';
 
 export default function Landing() {
-  const user = useLoaderData<typeof loader>();
-
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl">Welcome !</h1>
-      <h2 className="text-2xl pt-6">You are connected with {user.email}</h2>
-      <Form noValidate className="pt-6" method="POST" action="/signout">
-        <div>
-          <button
-            type="submit"
-            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Sign out
-          </button>
+    <div className="px-8 max-w-[1280px] m-auto">
+      <div className="flex items-center h-24">
+        <h1 className="font-heading text-4xl">Naia.</h1>
+      </div>
+      <div className="flex items-center justify-center mt-24">
+        <div className="w-1/2">
+          <h2 className="bg-gray-50 py-1 px-2 border rounded inline-flex gap-2 mb-6">
+            Participez a notre beta dès maintenant <ArrowRight />
+          </h2>
+          <h1 className="text-8xl font-heading leading-tight">
+            Retrouvez <br /> le plaisir d’offrir
+          </h1>
+          <div className="mt-2 flex gap-4">
+            <button className="bg-main px-6 py-4 rounded text-white">
+              Découvrez nous
+            </button>
+            <button className="bg-gray-50 border px-6 py-4 rounded">
+              Visitez notre blog
+            </button>
+          </div>
         </div>
-      </Form>
+        <div className="w-1/2 relative">
+          <div className="flex items-center gap-4 rounded bg-white p-4 shadow-lg absolute top-8 -left-32 max-w-md">
+            <img
+              src="/images/smiling.svg"
+              alt="face smiling"
+              className="w-12 h-12"
+            />
+            <div>
+              <p className="font-heading">Offrez avec le sourrir</p>
+              <p className="">
+                Finis les cadeaux par défaut, trouvez la bonne idée pour la
+                bonne personne.
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center gap-4 rounded bg-white p-4 shadow-lg absolute bottom-16 -right-48 max-w-md">
+            <img
+              src="/images/loving.svg"
+              alt="face smiling"
+              className="w-12 h-12"
+            />
+            <div>
+              <p className="font-heading">Provoquez la surprise</p>
+              <p className="">
+                Un cadeau surprenant est un cadeau réussi, ne manque plus aucune
+                occasion de surprendre.
+              </p>
+            </div>
+          </div>
+          <img
+            src="/images/giving.webp"
+            alt="man giving a gift to woman"
+            className="rounded-lg w-5/6 m-auto"
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,17 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Jost', 'sans-serif'],
+        heading: ['Mackinac', 'sans-serif'],
+      },
+      colors: {
+        dark: '#2A2D32',
+        main: '#048C8C',
+        secondary: '#FFB480',
+      },
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 } satisfies Config;
