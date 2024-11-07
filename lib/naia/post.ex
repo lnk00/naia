066422ba2a9ac68e868b8f90af425ce31691f27c
Damien Dumontet <damien.dumontet@protonmail.com>
2024-11-07 15:12:14 -0300

    create post schema

---
+++ lib/naia/post.ex
@@ -0,0 +1,19 @@
+defmodule Naia.Post do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "posts" do
+    field :author, :string
+    field :content, :string
+    field :title, :string
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(post, attrs) do
+    post
+    |> cast(attrs, [:title, :author, :content])
+    |> validate_required([:title, :author, :content])
+  end
+end
---
+++ priv/repo/migrations/20241107175411_create_posts.exs
@@ -0,0 +1,13 @@
+defmodule Naia.Repo.Migrations.CreatePosts do
+  use Ecto.Migration
+
+  def change do
+    create table(:posts) do
+      add :title, :string
+      add :author, :string
+      add :content, :string
+
+      timestamps(type: :utc_datetime)
+    end
+  end
+end
