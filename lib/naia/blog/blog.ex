725f010584a66c3a38952ac4f78fa03e46dfa71b
Damien Dumontet <damien.dumontet@protonmail.com>
2024-11-08 16:17:35 -0300

    insert new blog post

--- lib/naia/blog/blog.ex
+++ lib/naia/blog/blog.ex
@@ -2,7 +2,19 @@ defmodule Naia.Blog do
   alias Naia.Repo
   alias Naia.Blog.Post
 
+  @doc """
+  Return a lis of all blog posts published by all authors
+  """
+  @spec get_all_posts() :: list(Post)
   def get_all_posts do
     Post |> Repo.all()
+  end
+
+  @doc """
+  Insert a blog post in db
+  """
+  @spec insert_post(post :: Post) :: {:ok, Post} | {:error, Post.changeset()}
+  def insert_post(post) do
+    Repo.insert(post)
   end
 end
--- lib/naia_web/live/admin/upload.ex
+++ lib/naia_web/live/admin/upload.ex
@@ -1,7 +1,15 @@
 defmodule NaiaWeb.Admin.Upload do
+  alias Naia.Blog.Post
+  alias Naia.Blog
   use NaiaWeb, :live_view_admin
 
-  def handle_event("go_to_admin", _, socket) do
+  def handle_event("upload_post", _, socket) do
+    Blog.insert_post(%Post{
+      title: "title generated",
+      content: "content generated",
+      author: "Jannai Kalla"
+    })
+
     {:noreply, push_navigate(socket, to: "/admin")}
   end
 end
